0a97bb1e551ea6d5814f01514c8d677589fd8009
H6332 <yuchenghuang0628@gmail.com>
2025-11-12 11:16:07 +0800

    Add Max, Min
    新增最高分與最低分計算

--- Score.py
+++ Score.py
@@ -4,3 +4,12 @@ for i in score:
     if (int(i) < 60):
         count += 1
 print(count)
+min = int(score[0])
+max = int(score[0])
+for i in score:
+    if (int(i) < min):
+        min = int(i)
+    if (int(i) > max):
+        max = int(i)
+print(max)
+print(min)
